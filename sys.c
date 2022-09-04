84ab76ee269a9001b05fb5afe47e99bcd5e32b1e
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2022-09-04 16:49:26 -0500

    Fix `nolibc_mmap`

--- sys.c
+++ sys.c
@@ -1,6 +1,7 @@
 #include <sys/signal.h>
-#include <sys/syscall.h>
+#include <asm/unistd.h>
 #include <sys/mman.h>
+#include <asm/errno.h>
 #include "sys.h"
 #include "arch.h"
 
@@ -11,21 +12,33 @@ int* const nolibc___errno_location() {
 }
 
 __attribute__((noreturn)) int nolibc_abort() {
-  int pid = syscall0(SYS_getpid);
-  syscall2(SYS_kill, pid, SIGABRT);
+  int pid = syscall0(__NR_getpid);
+  syscall2(__NR_kill, pid, SIGABRT);
   for (;;);
 }
 
 void* nolibc_mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset) {
-  void* ret = (void*)syscall6(SYS_mmap, addr, length, prot, flags, fd, offset);
+#ifndef syscall6
+  errno = -ENOSYS;
+  return MAP_FAILED;
+#else
+  int n;
+#if defined(__i386__)
+	n = __NR_mmap2;
+	offset >>= 12;
+#else
+	n = __NR_mmap;
+#endif
+  void* ret = (void*)syscall6(n, addr, length, prot, flags, fd, offset);
   if ((unsigned long)ret >= -4095UL) {
     errno = -(long)ret;
     ret = MAP_FAILED;
   }
   return ret;
+#endif
 }
 int nolibc_munmap(void* addr, size_t length) {
-  int ret = syscall2(SYS_munmap, addr, length);
+  int ret = syscall2(__NR_munmap, addr, length);
   if (ret < 0) {
     errno = -ret;
     ret = -1;
