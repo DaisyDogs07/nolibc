278b017053b57a503c7c2770e0d0d241241aa5cf
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2022-09-05 01:21:03 -0500

    Add rest of syscall functions part 1

--- sys.c
+++ sys.c
@@ -17,9 +17,67 @@ __attribute__((noreturn)) int nolibc_abort() {
   for (;;);
 }
 
+void* nolibc_brk(void* addr) {
+  void* ret = (void*)syscall1(__NR_brk, addr);
+  if (!ret) {
+    errno = ENOMEM;
+    ret = (void*)-1;
+  }
+  return ret;
+}
+void* nolibc_sbrk(intptr_t incr) {
+  void* ret = (void*)syscall1(__NR_brk, 0);
+  if (ret && ((void*)syscall1(__NR_brk, ret + incr)) == ret + incr)
+    return ret + incr;
+  errno = ENOMEM;
+  return (void*)-1;
+}
+int nolibc_chdir(const char* path) {
+  int ret = syscall1(__NR_chdir, path);
+  if (ret < 0) {
+    errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_fchdir(int fd) {
+  int ret = syscall1(__NR_fchdir, fd);
+  if (ret < 0) {
+    errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_chmod(const char* path, mode_t mode) {
+  int ret = syscall2(__NR_chmod, path, mode);
+  if (ret < 0) {
+    errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_fchmod(int fd, mode_t mode) {
+  int ret = syscall2(__NR_fchmod, fd, mode);
+  if (ret < 0) {
+    errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+// TODO: Implement `atexit` and `on_exit`. And hopefully not let procrastination get in the way
+__attribute__((noreturn)) void nolibc_exit(int status) {
+  syscall1(__NR_fsync(0)); // in
+  syscall1(__NR_fsync(1)); // out
+  syscall1(__NR_fsync(2)); // err
+  syscall1(__NR_exit, status & 0377);
+  for (;;);
+}
+int nolibc_getpid() {
+  return syscall0(__NR_getpid);
+}
 void* nolibc_mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset) {
 #ifndef syscall6
-  errno = -ENOSYS;
+  errno = ENOSYS;
   return MAP_FAILED;
 #else
   int n;
--- sys.h
+++ sys.h
@@ -1,7 +1,16 @@
 #include <sys/types.h>
 #include <stddef.h>
+#include <stdint.h>
 
 int* const nolibc___errno_location();
 int nolibc_abort();
+void* nolibc_brk(void*);
+void* nolibc_sbrk(intptr_t);
+int nolibc_chdir(const char*);
+int nolibc_fchdir(int);
+int nolibc_chmod(const char*, mode_t);
+int nolibc_fchmod(int, mode_t);
+void nolibc_exit(int);
+int nolibc_getpid();
 void* nolibc_mmap(void*, size_t, int, int, int, off_t);
 int nolibc_munmap(void*, size_t);
