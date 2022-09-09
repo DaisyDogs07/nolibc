1775b0c67b793b92d9eaa38b39cdd0b7cf707bf9
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2022-09-09 16:15:03 -0500

    Add rest of syscall functions part 2

--- sys.c
+++ sys.c
@@ -1,14 +1,16 @@
-#include <sys/signal.h>
+#include <asm/errno.h>
 #include <asm/unistd.h>
 #include <sys/mman.h>
-#include <asm/errno.h>
-#include "sys.h"
+#include <sys/signal.h>
+#include <sys/times.h>
+#include <time.h>
 #include "arch.h"
+#include "sys.h"
 
-__thread int errno;
+__thread int nolibc_errno;
 
 int* const nolibc___errno_location() {
-  return &errno;
+  return &nolibc_errno;
 }
 
 __attribute__((noreturn)) int nolibc_abort() {
@@ -17,10 +19,69 @@ __attribute__((noreturn)) int nolibc_abort() {
   for (;;);
 }
 
+int nolibc_accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) {
+  int ret = syscall3(__NR_accept, sockfd, addr, addrlen);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_accept4(int sockfd, struct sockaddr* addr, socklen_t* addrlen, int flags) {
+  int ret = syscall4(__NR_accept4, sockfd, addr, addrlen, flags);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_access(const char* pathname, int mode) {
+  int ret = syscall2(__NR_access, pathname, mode);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_acct(const char* filename) {
+  int ret = syscall1(__NR_acct, filename);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_adjtimex(struct timex* buf) {
+  int ret = syscall1(__NR_adjtimex, buf);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_adjtime(const struct timeval* itv, struct timeval* otv) {
+  struct timex tntx;
+  if (itv) {
+    tntx.modes = ADJ_OFFSET_SINGLESHOT;
+    tntx.offset = itv->tv_sec * 1000000 + itv->tv_usec;
+  } else tntx.modes = ADJ_OFFSET_SS_READ;
+  if (nolibc_clock_adjtime(CLOCK_REALTIME, &tntx) < 0)
+    return -1;
+  if (otv) {
+    if (tntx.offset < 0) {
+      otv->tv_sec = -(-tntx.offset / 1000000);
+      otv->tv_usec = -(-tntx.offset % 1000000);
+    } else {
+      otv->tv_sec = tntx.offset / 1000000;
+      otv->tv_usec = tntx.offset % 1000000;
+    }
+  }
+  return 0;
+}
 void* nolibc_brk(void* addr) {
   void* ret = (void*)syscall1(__NR_brk, addr);
   if (!ret) {
-    errno = ENOMEM;
+    nolibc_errno = ENOMEM;
     ret = (void*)-1;
   }
   return ret;
@@ -29,13 +90,13 @@ void* nolibc_sbrk(intptr_t incr) {
   void* ret = (void*)syscall1(__NR_brk, 0);
   if (ret && ((void*)syscall1(__NR_brk, ret + incr)) == ret + incr)
     return ret + incr;
-  errno = ENOMEM;
+  nolibc_errno = ENOMEM;
   return (void*)-1;
 }
 int nolibc_chdir(const char* path) {
   int ret = syscall1(__NR_chdir, path);
   if (ret < 0) {
-    errno = -ret;
+    nolibc_errno = -ret;
     ret = -1;
   }
   return ret;
@@ -43,7 +104,7 @@ int nolibc_chdir(const char* path) {
 int nolibc_fchdir(int fd) {
   int ret = syscall1(__NR_fchdir, fd);
   if (ret < 0) {
-    errno = -ret;
+    nolibc_errno = -ret;
     ret = -1;
   }
   return ret;
@@ -51,7 +112,7 @@ int nolibc_fchdir(int fd) {
 int nolibc_chmod(const char* path, mode_t mode) {
   int ret = syscall2(__NR_chmod, path, mode);
   if (ret < 0) {
-    errno = -ret;
+    nolibc_errno = -ret;
     ret = -1;
   }
   return ret;
@@ -59,25 +120,131 @@ int nolibc_chmod(const char* path, mode_t mode) {
 int nolibc_fchmod(int fd, mode_t mode) {
   int ret = syscall2(__NR_fchmod, fd, mode);
   if (ret < 0) {
-    errno = -ret;
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_clock_adjtime(clockid_t clk, struct timex* tx) {
+  int ret = syscall2(__NR_clock_adjtime, clk, tx);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_clock_getcpuclockid(pid_t pid, clockid_t* clk) {
+  const clockid_t pidclock = (~pid << 3) | 2;
+  int ret = syscall2(__NR_clock_getres, pidclock, NULL);
+  if (ret == -EINVAL)
+    return ESRCH;
+  if (ret == 0) {
+    *clk = pidclock;
+    return 0;
+  }
+  return -ret;
+}
+int nolibc_clock_getres(clockid_t clk, struct timespec* res) {
+  int ret = syscall2(__NR_clock_getres, clk, res);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_clock_gettime(clockid_t clk, struct timespec* tp) {
+  int ret = syscall2(__NR_clock_gettime, clk, tp);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_clock_nanosleep(clockid_t clk, int flags, const struct timespec* rqtp, struct timespec* rmtp) {
+  return -syscall4(__NR_clock_nanosleep, clk, flags, rqtp, rmtp);
+}
+int nolibc_clock_settime(clockid_t clk, const struct timespec* tp) {
+  int ret = syscall2(__NR_clock_settime, clk, tp);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+clock_t nolibc_clock() {
+  struct timespec ts;
+  if (syscall2(__NR_clock_gettime, CLOCK_PROCESS_CPUTIME_ID, &ts) != 0)
+    return -1;
+  return ts.tv_sec * CLOCKS_PER_SEC + ts.tv_nsec / (1000000000 / CLOCKS_PER_SEC);
+}
+void nolibc_close(int fd) {
+  syscall1(__NR_close, fd);
+}
+int nolibc_connect(int fd, const struct sockaddr* addr, socklen_t len) {
+  int ret = syscall3(__NR_connect, fd, addr, len);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+ssize_t nolibc_copy_file_range(int fd_in, off_t* off_in, int fd_out, off_t* off_out, size_t len, unsigned int flags) {
+  int ret = syscall6(__NR_copy_file_range, fd_in, off_in, fd_out, off_out, len, flags);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_creat(const char* path, mode_t mode) {
+  int ret;
+#ifdef __NR_creat
+  ret = syscall2(__NR_creat, path, mode);
+#else
+  ret = syscall3(__NR_open, path, O_CREAT | O_WRONLY | O_TRUNC, mode);
+#endif
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_epoll_pwait(int fd, struct epoll_event* events, int maxevents, int timeout, const sigset_t* sigmask) {
+  int ret = syscall6(__NR_epoll_pwait, fd, events, maxevents, timeout, sigmask, sizeof(sigset_t));
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_eventfd(unsigned int initval, int flags) {
+  int ret = syscall2(__NR_eventfd2, initval, flags);
+  if (ret < 0) {
+    nolibc_errno = -ret;
     ret = -1;
   }
   return ret;
 }
 // TODO: Implement `atexit` and `on_exit`. And hopefully not let procrastination get in the way
-__attribute__((noreturn)) void nolibc_exit(int status) {
-  syscall1(__NR_fsync, 0); // in
-  syscall1(__NR_fsync, 1); // out
-  syscall1(__NR_fsync, 2); // err
-  syscall1(__NR_exit, status & 0377);
+__attribute__((noreturn)) void nolibc__exit(int status) {
+  syscall1(__NR_exit_group, status);
+  syscall1(__NR_exit, status);
   for (;;);
 }
 int nolibc_getpid() {
   return syscall0(__NR_getpid);
 }
+int nolibc_kill(pid_t pid, int sig) {
+  int ret = syscall2(__NR_kill, pid, sig);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
 void* nolibc_mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset) {
 #ifndef syscall6
-  errno = ENOSYS;
+  nolibc_errno = ENOSYS;
   return MAP_FAILED;
 #else
   int n;
@@ -89,7 +256,7 @@ void* nolibc_mmap(void* addr, size_t length, int prot, int flags, int fd, off_t 
 #endif
   void* ret = (void*)syscall6(n, addr, length, prot, flags, fd, offset);
   if ((unsigned long)ret >= -4095UL) {
-    errno = -(long)ret;
+    nolibc_errno = -(long)ret;
     ret = MAP_FAILED;
   }
   return ret;
@@ -98,7 +265,33 @@ void* nolibc_mmap(void* addr, size_t length, int prot, int flags, int fd, off_t 
 int nolibc_munmap(void* addr, size_t length) {
   int ret = syscall2(__NR_munmap, addr, length);
   if (ret < 0) {
-    errno = -ret;
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+int nolibc_pause() {
+  int ret;
+#ifdef __NR_pause
+  ret = syscall0(__NR_pause);
+#else
+  ret = syscall4(__NR_ppoll, NULL, 0, NULL, NULL);
+#endif
+  nolibc_errno = -ret;
+  return -1;
+}
+ssize_t nolibc_read(int fd, void* buf, size_t count) {
+  ssize_t ret = syscall3(__NR_read, fd, buf, count);
+  if (ret < 0) {
+    nolibc_errno = -ret;
+    ret = -1;
+  }
+  return ret;
+}
+ssize_t nolibc_write(int fd, const char* buf, size_t count) {
+  ssize_t ret = syscall3(__NR_write, fd, buf, count);
+  if (ret < 0) {
+    nolibc_errno = -ret;
     ret = -1;
   }
   return ret;
--- sys.h
+++ sys.h
@@ -1,16 +1,42 @@
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/timex.h>
 #include <sys/types.h>
 #include <stddef.h>
 #include <stdint.h>
 
 int* const nolibc___errno_location();
-int nolibc_abort();
+__attribute__((noreturn)) int nolibc_abort();
+int nolibc_accept(int, struct sockaddr*, socklen_t*);
+int nolibc_accept4(int, struct sockaddr*, socklen_t*, int);
+int nolibc_access(const char*, int);
+int nolibc_acct(const char*);
+int nolibc_adjtimex(struct timex*);
+int nolibc_adjtime(const struct timeval*, struct timeval*);
 void* nolibc_brk(void*);
 void* nolibc_sbrk(intptr_t);
 int nolibc_chdir(const char*);
 int nolibc_fchdir(int);
 int nolibc_chmod(const char*, mode_t);
 int nolibc_fchmod(int, mode_t);
-void nolibc_exit(int);
+int nolibc_clock_adjtime(clockid_t, struct timex*);
+int nolibc_clock_getcpuclockid(pid_t, clockid_t*);
+int nolibc_clock_getres(clockid_t, struct timespec*);
+int nolibc_clock_gettime(clockid_t, struct timespec*);
+int nolibc_clock_nanosleep(clockid_t, int, const struct timespec*, struct timespec*);
+int nolibc_clock_settime(clockid_t, const struct timespec*);
+clock_t nolibc_clock();
+void nolibc_close(int);
+int nolibc_connect(int, const struct sockaddr*, socklen_t);
+ssize_t nolibc_copy_file_range(int, off_t*, int, off_t*, size_t, unsigned int);
+int nolibc_creat(const char*, mode_t);
+int nolibc_epoll_pwait(int, struct epoll_event*, int, int, const sigset_t*);
+int nolibc_eventfd(unsigned int, int);
+__attribute__((noreturn)) void nolibc__exit(int);
 int nolibc_getpid();
+int nolibc_kill(pid_t, int);
 void* nolibc_mmap(void*, size_t, int, int, int, off_t);
 int nolibc_munmap(void*, size_t);
+int nolibc_pause();
+ssize_t nolibc_read(int, void*, size_t);
+ssize_t nolibc_write(int, const char*, size_t);
